7365bf46c96ea6fd038faf725c8d79d6e4f8d211
minheeH <43190252+minheeH@users.noreply.github.com>
2019-01-03 13:28:50 +0900

    temper

--- src/main/java/com/kh/mhm/common/util/Utils.java
+++ src/main/java/com/kh/mhm/common/util/Utils.java
@@ -12,7 +12,7 @@ public class Utils {
 		
 		//총페이지수 구하기
 		int totalPage = (int)Math.ceil((double)totalContents/numPerPage);
-		
+		if(totalPage ==0) totalPage=1;
 		//1.pageBar작성
 		//pageBar순회용변수 
 		int pageNo = ((cPage - 1)/pageBarSize) * pageBarSize +1;
